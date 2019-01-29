7fc52e324095c1a763220a9e63dd2e9b08b38703
Clara Munro and Tanner Damron <clara@>
2019-01-29 15:08:56 -0800

    add functional program that takes input from user and returns movie ticket price based on inputs using if statements and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,125 +1,52 @@
-// Business Logic for AddressBook
-function AddressBook() {
-  this.contacts = [],
-  this.currentId = 0
+// Business Logic
+function Ticket(movie, time, age) {
+  this.movie = movie,
+  this.time = time,
+  this.age = age
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts.push(contact);
-}
-
-Contact.prototype.addAddress = function(address) {
-  this.addresses.push(address);
-}
-
-AddressBook.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-AddressBook.prototype.findContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        return this.contacts[i];
-      }
-    }
-  };
-  return false;
-}
-
-AddressBook.prototype.deleteContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        delete this.contacts[i];
-        return true;
-      }
-    }
-  };
-  return false;
-}
-
-
-
-// Business logic for Contacts
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.phoneNumber = phoneNumber,
-  this.addresses = [];
-  console.log(this.addresses)
+Ticket.prototype.outputLogic = function(newTicket) {
+  if (newTicket.movie != "pokemonIMAX" && newTicket.age === "over55" ) {
+    $("#priceOne").show();
+    $("#priceTwo").hide();
+    $("#priceThree").hide();
+    $("#priceFour").hide();
+  } else if (newTicket.movie != "pokemonIMAX" && newTicket.age === "under18" && newTicket.time === "am") {
+    $("#priceTwo").show();
+    $("#priceOne").hide();
+    $("#priceThree").hide();
+    $("#priceFour").hide();
+  } else if (newTicket.movie != "pokemonIMAX" && newTicket.age === "over18" || newTicket.age === "pm") {
+    $("#priceThree").show();
+    $("#priceTwo").hide();
+    $("#priceOne").hide();
+    $("#priceFour").hide();
+  } else if (newTicket.movie === "pokemonIMAX") {
+    $("#priceFour").show();
+    $("#priceThree").hide();
+    $("#priceTwo").hide();
+    $("#priceOne").hide();
+  } else {
+    $("#priceTwo").show();
+    $("#priceOne").hide();
+    $("#priceThree").hide();
+    $("#priceFour").hide();
   }
-
-  function Address(email, address) {
-    this.email = email,
-    this.address = address
-  }
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
 }
 
+// User Interface Logic
+var ticket = new Ticket();
 
-// User Interface Logic -------
-var addressBook = new AddressBook();
-
-function displayContactDetails(addressBookToDisplay) {
-  var contactsList = $("ul#contacts");
-  var htmlForContactInfo = "";
-  addressBookToDisplay.contacts.forEach(function(contact){
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
-  });
-  contactsList.html(htmlForContactInfo);
-};
-
-function showContact(contactId) {
-  var contact = addressBook.findContact(contactId);
-  $("#show-contact").show();
-  $(".first-name").html(contact.firstName);
-  $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber);
-  var buttons = $("#buttons");
-  buttons.empty();
-  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete Contact</button>");
-}
-
-function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
-    console.log("The id of this <li> is " + this.id + ".");
-    showContact(this.id);
-  });
-  $("#buttons").on("click", ".deleteButton", function () {
-    addressBook.deleteContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
-  });
-};
-
-
-$(document).ready(function () {
-  attachContactListeners();
-  $("form#new-contact").submit(function (event) {
+$(document).ready(function() {
+  $("form#tickets").submit(function(event) {
     event.preventDefault();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedPhoneNumber = $("input#new-phone-number").val();
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-phone-number").val("");
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-    addressBook.addContact(newContact);
-    displayContactDetails(addressBook);
-    console.log(addressBook.contacts);
+    var inputtedMovie = $("input:radio[name=movie]:checked").val();
+    var inputtedTime = $("input:radio[name=time]:checked").val();
+    var inputtedAge = $("input:radio[name=age]:checked").val();
+    var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
+    console.log(newTicket);
+    ticket.outputLogic(newTicket);
+
   });
-  $("form#addressForm").submit(function(event) {
-    event.preventDefault();
-    var inputtedEmail = $("input#new-email-address").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newAddress = new Address(inputtedEmail, inputtedAddress);
-    addresses.addAddress(newAddress);
-    displayContactDetails(addressBook);
-    console.log(added);
-  });
+
 });
